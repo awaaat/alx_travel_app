ef1fb2e6bb42938ebc7d8bffd5195ef762d7085d
Allan <awaliaulah65@gmail.com>
2025-05-18 23:44:57 +0300

    just added more

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -16,7 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ALX Travel App API",
+        default_version="v1",
+        description="Official APA documentation for ALX Travel App"
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
